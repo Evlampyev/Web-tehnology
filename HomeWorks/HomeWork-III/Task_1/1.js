c63dffc598afeb91432596a04ec90a0d25a37577
Bad_S <Evlampyev.Alex@gmail.com>
2023-01-10 00:18:36 +0500

    homework-3 ready

---
+++ HomeWorks/HomeWork-III/Task_1/1.js
@@ -0,0 +1,3 @@
+const temperatureInCelsius = prompt("Введите температуру в Цельсиях")
+const temperatureInFahrenheit = (9 / 5) * temperatureInCelsius + 32
+alert(`Цельсий: ${temperatureInCelsius} = Фаренгейт ${temperatureInFahrenheit.toFixed(1)}`)
---
+++ HomeWorks/HomeWork-III/Task_2/2.js
@@ -0,0 +1,5 @@
+function greeting(user_name) {
+    console.log(`Привет, ${user_name}!`)
+}
+
+greeting(prompt("Как вас зовут? "))
